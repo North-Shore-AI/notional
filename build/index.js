2df5285418c69ba155f2aac1b7f1112ae49812cd
Matt Casey <mattwad@gmail.com>
2020-11-19 21:11:58 -0700

    remove built files

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var notional_1 = require("./lib/notional");
-exports.default = notional_1.default;
-exports.Notional = notional_1.Notional;
--- build/lib/table/types.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Filters = {};
-export declare type UpdateData = {
-    id: string;
-    data: object[];
-};
