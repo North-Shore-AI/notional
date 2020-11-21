8a6d2950b45f65c7397af5d8c846a086535eef05
Matt Casey <mattwad@gmail.com>
2020-11-21 00:07:39 -0700

    fix ts error

--- src/lib/transaction-manager/index.ts
+++ src/lib/transaction-manager/index.ts
@@ -161,20 +161,18 @@ export default class TransactionManager {
     return await this.submitTransaction(transactions);
   }
 
-  public async insert(
-    data: { id: string; type: string; value: string | string[] }[][],
-  ) {
+  public async insert(data: object[][]) {
     const now = new Date().getTime();
 
     const newBlockIds = data.map(
-      rows => rows.find(r => r.id === 'id')?.value || uuid(),
+      (rows: any[]) => rows.find((r: any) => r.id === 'id')?.value || uuid(),
     );
 
     const dataToInsert = data.map((rows, index) => ({
       id: uuid(),
       operations: rows
-        .filter(row => row.id !== 'id')
-        .map(entry => ({
+        .filter((entry: any) => entry.id !== 'id')
+        .map((entry: any) => ({
           id: newBlockIds[index],
           table: 'block',
           path: ['properties', entry.id],
